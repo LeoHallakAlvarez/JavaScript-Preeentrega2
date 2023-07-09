08d1778bef343bc69bce5dbb4f834da31249a407
LeoHallakAlvarez <detroitborg92@hotmail.com>
2023-07-09 00:07:44 -0300

    agregando un poco de estilo

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 alert("Bienvenidos a MotoMundo-18")
-console.log(motos);
+prompt("para ingresar al sitio debe ser mayor de edad, tiene mas de 18? Ingrese su edad a continuación")
+
+// console.log(motos);
+
+const dataBusqueda = {
+    marca:'',
+    modelo:'',
+    año:'',
+    minimo:'',
+    maximo:'',
+    precio:'',
+    color:'',
+
+}
+
+function mostrarMotos(motos){
+    motos.forEach( moto =>{
+        console.log(moto.marca + " " + moto.modelo +" ")
+    })
+        
+    };
 
 
 
