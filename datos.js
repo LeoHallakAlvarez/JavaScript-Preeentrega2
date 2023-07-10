ebdcbdc6fe1439e6d43a1902d92610b0ec009571
LeoHallakAlvarez <detroitborg92@hotmail.com>
2023-07-09 22:56:43 -0300

    listo por hoy

--- datos.js
+++ datos.js
@@ -26,4 +26,81 @@ const motos = [
     año:'2010',
     precio:'80000',
     color:'negro',
-} ]
+},
+{
+    marca:'Yamaha',
+    modelo:'KTP-70',
+    año:'2002',
+    precio:'140000',
+    color:'rojo',
+}, 
+{
+    marca:'Derbi',
+    modelo:'ALM-88',
+    año:'2011',
+    precio:'90000',
+    color:'verder',
+},
+{
+    marca:'Honda',
+    modelo:'BB-E2',
+    año:'2015',
+    precio:'145000',
+    color:'blanco',
+},
+{
+    marca:'Honda',
+    modelo:'AB-32',
+    año:'2008',
+    precio:'38000',
+    color:'celeste',
+},
+{
+    marca:'Ducatti',
+    modelo:'UTJ-98',
+    año:'2019',
+    precio:'200000',
+    color:'amarillo',
+},
+{
+    marca:'Victory',
+    modelo:'FR-789',
+    año:'2018',
+    precio:'50000',
+    color:'verde',
+},
+{
+    marca:'Derbi',
+    modelo:'AG-T78',
+    año:'2006',
+    precio:'60000',
+    color:'azul',
+},
+{
+    marca:'Bsa',
+    modelo:'DKJ-777',
+    año:'2009',
+    precio:'25000',
+    color:'naranja',
+},
+{
+    marca:'Kawasaki',
+    modelo:'KW-444',
+    año:'2020',
+    precio:'195000',
+    color:'gris',
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
--- main.js
+++ main.js
@@ -4,7 +4,7 @@ prompt("para ingresar al sitio debe ser mayor de edad, tiene mas de 18? Ingrese 
 // console.log(motos);
 
 const dataBusqueda = {
-    marca:'',
+    marca:'Brosetti',
     modelo:'',
     año:'',
     minimo:'',
@@ -16,10 +16,75 @@ const dataBusqueda = {
 
 function mostrarMotos(motos){
     motos.forEach( moto =>{
-        console.log(moto.marca + " " + moto.modelo +" ")
+        console.log(moto.marca + " " + moto.modelo +" " + moto.año + " " + moto.color +" " + moto.precio)
     })
         
-    };
+    }
+
+    function filtrarPorMarca(moto){
+        if(dataBusqueda.marca){
+            return moto.marca === dataBusqueda.marca
+        }
+        return moto;
+    }
+
+    
+    function filtrarPorAño(moto){
+        if(dataBusqueda.año){
+            return moto.año === dataBusqueda.año
+        }
+        return moto;
+    }
+
+
+    
+    function filtrarPorMinimo(moto){
+        if(dataBusqueda.minimo){
+            return moto.minimo >= dataBusqueda.minimo
+        }
+        return moto;
+    }
+
+    
+    function filtrarPorMaximo(moto){
+        if(dataBusqueda.maximo){
+            return moto.maximo <= dataBusqueda.maximo
+        }
+        return moto;
+    }
+
+
+    
+    function filtrarPorColor(moto){
+        if(dataBusqueda.color){
+            return moto.color === dataBusqueda.color
+        }
+        return moto;
+    }
+
+    
+    function filtrarPorPrecio(moto){
+        if(dataBusqueda.precio){
+            return moto.precio === dataBusqueda.precio
+        }
+        return moto;
+    }
+
+    function filtrarMoto(){
+        let resultado = motos.filter(filtrarMarca)
+        console.log(resultado.length)
+    }
+
+
+
+    filtrarMoto();
+
+
+
+
+
+
+    // mostrarMotos(motos)
 
 
 
